3d4711371dc99bceb51ab7003334256c8c45a7ac
Hrishikesh <hrishikesh2727@gmail.com>
2021-02-07 11:41:53 +0530

    Delete old code.

--- src/main/java/com/currencytrading/repository/OrderBookRepository.java
+++
@@ -1,9 +0,0 @@
-/*package com.currancytrading.repository;
-
-
-import com.currancytrading.entity.OrderBookDetails;
-import org.springframework.data.repository.CrudRepository;
-
-public interface OrderBookRepository extends CrudRepository<OrderBookDetails, String> {
-
-}*/
